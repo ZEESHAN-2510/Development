cbf609f5d8e856d64d62b5e13aabb64d19164e04
ZEESHAN-2510 <xeeshantahir2510@gmail.com>
2022-06-27 21:14:03 +0500

    soa-developement

---
+++ Dev/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">SOA</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
